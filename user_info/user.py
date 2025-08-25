ee694f7f7d5243f2e4012d7081e4d43bb1c9312a
Luigi Selmi <luigiselmi@gmail.com>
2025-08-25 16:52:09 +0200

    copernicus sentinels api setttings

---
+++ user_info/user.py
@@ -0,0 +1,15 @@
+# If you don't have any user/pass, register at https://dataspace.copernicus.eu
+# Username and Password below should be inside quotation marks ("").
+username = '************'
+password = '************'
+##============================================================================
+# Modify the variables below and apply your desired time period.
+start_year = 2025
+start_month = 6
+start_day = 1
+start_hour = 1
+##----------------------------------------------------------------------------
+end_year = 2025
+end_month = 6
+end_day = 30
+end_hour = 23
